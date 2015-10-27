33dc41ecf1bcc01c6fe926c7f9a65230d4187ef4
Fredrick Abayie <fredrick.abayie@gmail.com>
2015-10-27 21:19:31 +0000

    Cleaning files

--- inventory.php
+++ inventory.php
@@ -240,172 +240,12 @@ function make_transaction ( ) {
         if($make_transaction->make_transaction_query($customerNumber, $productId, $total)) {
             echo '{"result":1, "status": "Product Sold"}';
             if ($total >= 500) {
-                send_mesg( $customerNumber, "10% Discount on next purchase" );
+                send_mesg( $customerNumber, "Get 10% Discount on next purchase" );
             }
         }
         else {
             echo '{"result":0, "status": "Failed to sell product"}';
         }
-    }
-}
-
-
-
-
-
-
-    
-/**
-*Function to add a new student to the database
-*
-*/
-function register_student ( )
-{
-    if (isset($_REQUEST['message']))
-    {
-        include_once 'act.php';
-
-        $content = $_REQUEST['message'];
-        $message = explode(" ", $content);
-
-        $studentID = $message[0];
-        $studentName = $message[1];
-        $GPA = $message[2];
-        $Major = $message[3];
-        $phoneNo = $message[4];
-
-        $register = new Act();
-        
-        $message = "Registered";
-                
-        if ($register->register_student( $studentID, $studentName, $GPA, $Major, $phoneNo ))
-        {
-            send_mesg($phoneNo, $message);
-            echo "Registered";
-        }
-        else
-        {
-            echo "Please enter your registration details";
-        }
-    }
-}
-
-
-
-
-/**
-*Function to allow student to check GPA
-*
-*/
-function check_gpa ( )
-{
-    if (isset($_REQUEST['message']))
-    {
-        include_once 'act.php';
-        
-        $content = $_REQUEST['message'];
-        $message = explode(" ", $content);
-        $studentID = $message[0];
-        
-        $select = new Act();
-        
-        if ( $select->check_gpa( $studentID ))
-        {
-            $row = $select->fetch();
-            echo "GPA for ".$studentID." is: ".$row['GPA'];
-        }
-        else
-        {
-            echo "Failed to check gpa";
-        }
-    }
-    else
-    {
-        echo "Please enter your studentID";
-    }
-}
-
-
-
-
-/**
-*Function to allow student to check Major
-*
-*/
-function check_major ( )
-{
-    if (isset($_REQUEST['message']))
-    {
-        include_once 'act.php';
-        
-        $content = $_REQUEST['message'];
-        $message = explode(" ", $content);
-        $studentID = $message[0];
-        
-        $select = new Act();
-        
-        if ( $select->check_major( $studentID ))
-        {
-            $row = $select->fetch();
-            echo "Major for ".$studentID." is: ".$row['Major'];
-        }
-        else
-        {
-            echo "Failed to check major";
-        }
-    }
-    else
-    {
-        echo "Please enter your studentID";
-    }
-}
-
-
-
-/**
-*Function to give the user a one time password
-*
-*/
-function get_otp ( )
-{
-    if (isset($_REQUEST['message']))
-    {
-        include_once 'act.php';
-        session_start();
-         session_unset();
-        
-        $studentID = $_REQUEST['message'];
-        
-        $_SESSION['passcode'] = random();
-        
-        $phoneNo = new Act();
-        
-        if ($phoneNo->get_details ( $studentID ))
-        {
-            $row = $phoneNo->fetch();
-            
-            send_mesg ($row['phoneNo'],"Your passcode to login is:".$_SESSION['passcode']);
-            echo $row['phoneNo']." ".$_SESSION['passcode'];
-        }
-    }
-}
-
-
-
-/**
-*Function to display the details of a student
-*
-*/
-function display_details ( $studentID )
-{
-    include_once 'act.php';
-    
-    $detail = new Act();
-    
-    if ($detail->get_details ( $studentID ))
-    {
-        $row = $detail->fetch();
-        
     }
 }
 
@@ -431,11 +271,11 @@ function send_mesg( $phoneNo, $message )
         
         if($messageResponse instanceof MessageResponse)
         {
-            echo "msg1:".$messageResponse->getStatus()."</br></br>";
+//            echo "msg1:".$messageResponse->getStatus()."</br></br>";
         }
         elseif ($messageResponse instanceof HttpResponse)
         {
-            echo "\nServer Response Status: ".$messageResponse->getStatus()."</br></br>";
+//            echo "\nServer Response Status: ".$messageResponse->getStatus()."</br></br>";
         }
         
         echo "</br>success done";
